3c484e11d1ff535fa4a1d4ec0cfd9aa60ce0cb1a
shymaahanynoor <139652535+shymaahanynoor@users.noreply.github.com>
2024-03-27 17:47:54 +0200

    update category

---
+++ E-Commerce/src/app/category/category.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AllCategoryComponent } from './components/all-category/all-category.component';
+
+
+
+@NgModule({
+  declarations: [
+    AllCategoryComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CategoryModule { }
